9ca165c81d39487e5a0762c35621f71a567efed3
damczi17 <72543223+damczi17@users.noreply.github.com>
2021-12-08 21:54:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <algorithm>
 
 #include "net.h"
 #include "element4.h"
@@ -9,95 +10,74 @@
 
 #define H 0.1 //Height of physical net
 #define B 0.1 //Width of physical net
-#define nH 5 //Height of net
-#define nB 5//Width of net
-#define N 2 //Number of integration points
+#define nH 4 //Height of net
+#define nB 4 //Width of net
+#define N 3 //Number of integration points
 #define conductivity 25 //Conductivity [W/(mC)]
+#define alfa 300 //Conductivity [W/(mC)]
 #define ambTemp 1200 //ambient temperature [C]
 #define c 700 //specific heat [J/(kgC)]
 #define ro 7800 //density [kg/m3]
 #define step 50 //simulation step time [s]
-#define initTemp 100 //initial temperature [C]
+#define time 500 //simulation time [s]
+#define initTemperature 100 //initial temperature [C]
 
 
 int main() {
 
-	grid net = netGenerate(H, B, nH, nB);
+	grid net = netGenerate(H, B, nH, nB, initTemperature);
 	element4_2D elem = derivate(N);
 
 	jakobianCnt(net, elem);
 
 	harraycnt(net, elem, conductivity);
 
-	wallsCnt(net, elem, conductivity, ambTemp);
+	wallsCnt(net, elem, alfa, ambTemp);
 
 	HbcCnt(net, elem);
 
-	std::vector<std::vector<double>> Hglobal, Cglobal;
-	std::vector<double> Pglobal;
+	globalMatices global1;
 
 	cmatrixCnt(net, elem, ro, c);
 
-	globalH(net, Hglobal, Pglobal, Cglobal);
+	agregation(net, global1.Hglobal, global1.Pglobal, global1.Cglobal);
 
-	std::cout << "\nMacierz H dla PC1:\n";
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			std::cout << net.elements[0].Hmatrix[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-	std::cout << "\nMacierz Hbc dla PC1:\n";
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			std::cout << net.elements[0].Hbc[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-	std::cout << "\nMacierz C dla PC1:\n";
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			std::cout << net.elements[0].Cmatrix[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
+	//for (int i = 0; i < net.nE; ++i) {
+	//	std::cout << "Element: " << i + 1 << std::endl;
+	//	for (int j = 0; j < 4; ++j) {
+	//		for (int k = 0; k < 4; ++k) {
+	//			std::cout << net.elements[i].Hbc[j][k] << " ";
+	//		}
+	//		std::cout << std::endl;
+	//	}
+	//	std::cout << std::endl;
+	//}
 	
-	for (int j = 0; j < net.nE; ++j) {
-		std::cout << "\nWektor P dla PC" << j+1 <<"\n";
-		for (int i = 0; i < 4; ++i) {
-			std::cout << net.elements[j].P[i] << "\n";
-		}
-	}
+	std::cout << net.elements[0].jak.det[0];
+	global1.showMatrices();
 
-	std::cout << "\n\nGlobal H + Hbc matrix\n";
-	for (int i = 0; i < net.nN; ++i) {
-		for (int j = 0; j < net.nN; ++j) {
-			std::cout << Hglobal[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	globalH_C(net, global1.Hglobal, global1.Cglobal, step);
 
-	std::cout << "\n\nGlobal C matrix\n";
-	for (int i = 0; i < net.nN; ++i) {
-		for (int j = 0; j < net.nN; ++j) {
-			std::cout << Cglobal[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	//std::cout << "\n\nGlobal H + C/dT matrix\n";
+	//for (int i = 0; i < net.nN; ++i) {
+	//	for (int j = 0; j < net.nN; ++j) {
+	//		std::cout << global1.Hglobal[i][j] << " ";
+	//	}
+	//	std::cout << std::endl;
+	//}
 
-	std::cout << "\n\nGlobal P vector\n";
-	for (int i = 0; i < net.nN; ++i) {
-		std::cout << Pglobal[i] << "\n ";
-	}
 
-	std::vector<double> tempSolution = equationSolve(Hglobal, Pglobal);
+	std::vector<double> tempSolution;
 
-	std::cout << "\n\nSolutions of temperature:\n";
-	for (int i = 0; i < net.nN; ++i) {
-		std::cout << "Temperature " << i+1 << " " << tempSolution[i] << "\n ";
+	for (int i = step; i <= time ; i+=step) {
+		std::cout << "\n--------------------------------------------\n";
+		
+		tempSolution = PvectorCnt(net, global1, step);
+		tempSolution = equationSolve(global1.Hglobal, tempSolution);
+
+		auto [min, max] = std::minmax_element(begin(tempSolution), end(tempSolution));
+		std::cout << "\nTime " << i << " sec temperature value: " << *min << " Max temperature value: " << *max << std::endl;
+		updateNodesTemp(net, tempSolution);
 	}
 
 	return 0;
